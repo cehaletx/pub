b7d01537b21941665d761116437d3ca6857b0ad7
Charlie Hale <charles.hale@elastic.co>
2023-08-14 14:38:30 -0500

    added generic env

---
+++ msmarco4/env.sh
@@ -0,0 +1,7 @@
+export ELASTIC_USERNAME=elastic
+export ELASTIC_PASSWORD=xxxxxxxxxx
+export ELASTIC_CLOUD_ID=xxxxxxxxxxx
+export RRF_URL=xxxxxxxxx
+export RRF_USERNAME=admin
+export RRF_PASSWORD=xxxxxxxxxx
+export OPENAI_API_KEY=xxxxxxxxxxxxxx
